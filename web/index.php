2b605a23731e2917100cfe75b778cab375443a85
Alexander Reitzel <funtimecoding@gmail.com>
2015-05-31 14:27:45 +0200

    no need for kernel instantiation anymore

--- web/index.php
+++ web/index.php
@@ -1,11 +1,10 @@
 <?php
 
-use FunTimeCoding\PhpUtility\Framework\Kernel;
+namespace FunTimeCoding\PhpUtility\Web;
+
 use FunTimeCoding\PhpUtility\PhpUtility\PhpUtility;
 
 require_once realpath(__DIR__).'/../vendor/autoload.php';
-$kernel = new Kernel();
-$kernel->load();
 
 $application = new PhpUtility();
 $application->main();
