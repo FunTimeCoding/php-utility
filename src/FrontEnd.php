6e92272730c73490524b0c23b5ba84ce12fe32ef
Alexander Reitzel <funtimecoding@gmail.com>
2018-03-25 19:53:49 +0200

    Complete form example configuration

--- src/FrontEnd.php
+++ src/FrontEnd.php
@@ -5,13 +5,26 @@ use DateTime;
 use FastRoute\Dispatcher;
 use FastRoute\RouteCollector;
 use function FastRoute\simpleDispatcher;
+use ReflectionClass;
+use ReflectionException;
+use Symfony\Bridge\Twig\AppVariable;
 use Symfony\Bridge\Twig\Extension\FormExtension;
+use Symfony\Bridge\Twig\Extension\TranslationExtension;
+use Symfony\Bridge\Twig\Form\TwigRendererEngine;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
 use Symfony\Component\Form\Extension\Core\Type\FormType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Validator\ValidatorExtension;
+use Symfony\Component\Form\FormRenderer;
 use Symfony\Component\Form\Forms;
-use Twig_Environment;
+use Symfony\Component\Translation\Loader\XliffFileLoader;
+use Symfony\Component\Translation\Translator;
+use Symfony\Component\Validator\Constraints\NotBlank;
+use Symfony\Component\Validator\Constraints\Type;
+use Symfony\Component\Validator\Validation;
+use Twig\Environment;
 use Twig_Error;
+use Twig_FactoryRuntimeLoader;
 use Twig_Loader_Filesystem;
 
 class FrontEnd
@@ -29,13 +42,67 @@ class FrontEnd
         echo $frontEnd->run($resourceIdentifier, $method);
     }
 
-    public function createTwigEnvironment(): Twig_Environment
+    /**
+     * @return Environment
+     * @throws ReflectionException
+     */
+    public function createTwigEnvironment(): Environment
     {
+        $appVariableReflection = new ReflectionClass(AppVariable::class);
         // TODO: Use cache.
         //['cache' => 'path/to/cache']
-        return new Twig_Environment(
-            new Twig_Loader_Filesystem(__DIR__ . '/../template')
+        $twig = new Environment(
+            new Twig_Loader_Filesystem(
+                [
+                    __DIR__ . '/../template',
+                    dirname($appVariableReflection->getFileName()) . '/Resources/views/Form',
+                ]
+            )
         );
+        $formEngine = new TwigRendererEngine(
+            [
+                'form_div_layout.html.twig'
+            ],
+            $twig
+        );
+        $twig->addRuntimeLoader(
+            new Twig_FactoryRuntimeLoader(
+                [
+                    FormRenderer::class => function () use ($formEngine) {
+                        return new FormRenderer($formEngine);
+                    },
+                ]
+            )
+        );
+        $twig->addExtension(new FormExtension());
+
+        $translator = new Translator('en');
+        $translator->addLoader('xlf', new XliffFileLoader());
+        $translator->addResource(
+            'xlf',
+            __DIR__ . '/../translation/messages.en.xlf',
+            'en'
+        );
+
+        $vendorDirectory = realpath(__DIR__ . '/../vendor');
+        $vendorFormDirectory = $vendorDirectory . '/symfony/form';
+        $vendorValidatorDirectory = $vendorDirectory . '/symfony/validator';
+        $translator->addResource(
+            'xlf',
+            $vendorFormDirectory . '/Resources/translations/validators.en.xlf',
+            'en',
+            'validators'
+        );
+        $translator->addResource(
+            'xlf',
+            $vendorValidatorDirectory . '/Resources/translations/validators.en.xlf',
+            'en',
+            'validators'
+        );
+
+        $twig->addExtension(new TranslationExtension($translator));
+
+        return $twig;
     }
 
     public function prepareResourceIdentifier(string $fullIdentifier): string
@@ -51,12 +118,14 @@ class FrontEnd
 
     public function run(string $resourceIdentifier, string $method): string
     {
-        $dispatcher = simpleDispatcher(function (RouteCollector $collector) {
-            $collector->addRoute(self::GET, '/', self::INDEX_HANDLER);
-            $collector->addRoute(self::GET, '/settings', self::SETTINGS_HANDLER);
-            $collector->addRoute(self::GET, '/form', self::FORM_GET_HANDLER);
-            $collector->addRoute(self::POST, '/form', self::FORM_POST_HANDLER);
-        });
+        $dispatcher = simpleDispatcher(
+            function (RouteCollector $collector) {
+                $collector->addRoute(self::GET, '/', self::INDEX_HANDLER);
+                $collector->addRoute(self::GET, '/settings', self::SETTINGS_HANDLER);
+                $collector->addRoute(self::GET, '/form', self::FORM_GET_HANDLER);
+                $collector->addRoute(self::POST, '/form', self::FORM_POST_HANDLER);
+            }
+        );
         $dispatchResult = $dispatcher->dispatch(
             $method,
             $this->prepareResourceIdentifier($resourceIdentifier)
@@ -75,38 +144,63 @@ class FrontEnd
                 $handler = $dispatchResult[1];
                 //$vars = $dispatchResult[2];
                 //echo var_export($vars, true) . PHP_EOL;
-                $twig = $this->createTwigEnvironment();
 
                 try {
+                    $twig = $this->createTwigEnvironment();
+
                     if ($handler == self::INDEX_HANDLER) {
                         $output = $twig->render('index.html.twig');
                     } elseif ($handler == self::SETTINGS_HANDLER) {
                         $output = $twig->render('settings.html.twig');
-                    } else if ($handler == self::FORM_GET_HANDLER) {
-                        $twig->addExtension(new FormExtension());
+                    } elseif ($handler == self::FORM_GET_HANDLER || $handler == self::FORM_POST_HANDLER) {
                         $factory = Forms::createFormFactoryBuilder()
-                            // TODO: Comment in once the form works.
-                            //->addExtension(new ValidatorExtension(Validation::createValidator()))
+                            ->addExtension(new ValidatorExtension(Validation::createValidator()))
                             ->getFormFactory();
                         $form = $factory->createBuilder(
                             FormType::class,
-                            ['dueDate' => new DateTime('tomorrow')]
+                            [
+                                'dueDate' => new DateTime('tomorrow')
+                            ]
                         )
-                            ->add('task', TextType::class)
-                            ->add('dueDate', DateType::class)
+                            ->add(
+                                'task',
+                                TextType::class,
+                                [
+                                    'constraints' => new NotBlank()
+                                ]
+                            )
+                            ->add(
+                                'dueDate',
+                                DateType::class,
+                                [
+                                    'constraints' => [
+                                        new NotBlank(),
+                                        new Type(DateTime::class)
+                                    ]
+                                ]
+                            )
                             ->getForm();
-                        $output = $twig->render(
-                            'form.html.twig',
-                            ['form' => $form->createView()]
-                        );
-                    } else if ($handler == self::FORM_POST_HANDLER) {
-                        $output = 'Implement once the FORM_GET_HANDLER works.';
+                        $form->handleRequest();
+
+                        if ($form->isSubmitted() && $form->isValid()) {
+                            $output = 'Task submitted: ' . $form->getData()['task'] . PHP_EOL;
+                        } else {
+                            $output = $twig->render(
+                                'form.html.twig',
+                                [
+                                    'form' => $form->createView()
+                                ]
+                            );
+                        }
+                    } elseif ($handler == self::FORM_POST_HANDLER) {
+                        $output = 'Implement once the FORM_GET_HANDLER works.' . PHP_EOL;
                     } else {
-                        $output = 'Handler not implemented: ' . $handler;
+                        $output = 'Handler not implemented: ' . $handler . PHP_EOL;
                     }
                 } catch (Twig_Error $exception) {
-                    //$output = 'Render error.' . PHP_EOL;
-                    $output = $exception->getMessage();
+                    $output = $exception->getMessage() . PHP_EOL;
+                } catch (ReflectionException $exception) {
+                    $output = $exception->getMessage() . PHP_EOL;
                 }
 
                 break;
