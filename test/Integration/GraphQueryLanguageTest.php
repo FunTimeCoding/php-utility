ff40f8168d27fedf9bc69a95e7e6721f78081b3c
Alexander Reitzel <funtimecoding@gmail.com>
2018-09-03 18:19:55 +0200

    add graphql library

---
+++ test/Integration/GraphQueryLanguageTest.php
@@ -0,0 +1,13 @@
+<?php
+namespace FunTimeCoding\PhpUtility\Test\Integration;
+
+use PHPUnit\Framework\TestCase;
+
+class GraphQueryLanguageTest extends TestCase
+{
+    public function testRequest()
+    {
+        // See: https://github.com/webonyx/graphql-php/blob/master/examples/00-hello-world/graphql.php
+        $this->fail('Not yet implemented.');
+    }
+}
