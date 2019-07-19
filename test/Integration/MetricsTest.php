5dedd6f2cfa37a7982d50d176126fcd06685f330
Alexander Reitzel <funtimecoding@gmail.com>
2019-07-19 11:07:31 +0200

    update metrics test from skeleton

--- test/Integration/MetricsTest.php
+++ test/Integration/MetricsTest.php
@@ -1,63 +1,94 @@
 <?php
-
 namespace FunTimeCoding\PhpUtility\Test\Integration;
 
 use DirectoryIterator;
+use Exception;
 use PHPUnit\Framework\TestCase;
 use RecursiveDirectoryIterator;
 use RecursiveIteratorIterator;
 
 class MetricsTest extends TestCase
 {
-    /**
-     * Wrong lowercase c in TestCase makes phploc miss that class.
-     */
-    public function testInheritanceCapitalization(): void
+    public static function isFile(DirectoryIterator $iterator): bool
     {
-        $testDirectory = realpath(__DIR__ . DIRECTORY_SEPARATOR . '..');
-        $this->assertStringStartsWith('/', $testDirectory);
-        $this->assertEquals('test', basename($testDirectory));
+        return $iterator->isFile();
+    }
 
+    public static function getFileName(DirectoryIterator $iterator): string
+    {
+        return $iterator->getFilename();
+    }
+
+    public static function getPathName(DirectoryIterator $iterator): string
+    {
+        return $iterator->getPathname();
+    }
+
+    /**
+     * @param string $testDirectory
+     * @return string[]
+     */
+    public static function collectFiles(string $testDirectory): array
+    {
         $files = [];
-        $directoryIterator = new RecursiveDirectoryIterator($testDirectory, RecursiveDirectoryIterator::SKIP_DOTS);
-        $iteratorIterator = new RecursiveIteratorIterator($directoryIterator, RecursiveIteratorIterator::SELF_FIRST);
+        $iteratorIterator = new RecursiveIteratorIterator(
+            new RecursiveDirectoryIterator(
+                $testDirectory,
+                RecursiveDirectoryIterator::SKIP_DOTS
+            ),
+            RecursiveIteratorIterator::SELF_FIRST
+        );
+
         foreach ($iteratorIterator as $item) {
-            /** @var DirectoryIterator $item */
-            if ($item->isFile()) {
-                $filename = $item->getFilename();
-                if ($this->endsWith($filename, 'Test.php')) {
-                    $files[] = $item->getPathname();
+            if (self::isFile($item)) {
+                $filename = self::getFileName($item);
+
+                if (self::endsWith($filename, 'Test.php')) {
+                    $files[] = self::getPathName($item);
                 }
             }
         }
 
-        foreach ($files as $file) {
-            $handle = fopen($file, 'r');
+        return $files;
+    }
 
-            if ($handle) {
+    /**
+     * Find wrongly capitalized TestCase with a lower case c, which causes problems with phploc.
+     * @throws Exception
+     */
+    public function testInheritanceCapitalization(): void
+    {
+        $testDirectory = '' . realpath(__DIR__ . DIRECTORY_SEPARATOR . '..');
+        self::assertStringStartsWith('/', $testDirectory);
+        self::assertEquals('test', basename($testDirectory));
+
+        foreach (self::collectFiles($testDirectory) as $file) {
+            $handle = fopen($file, 'rb');
+
+            if ($handle !== false) {
                 $found = false;
+
                 while (($line = fgets($handle)) !== false) {
-                    if ($this->startsWith($line, 'class ')) {
+                    if (self::startsWith('' . $line, 'class ')) {
                         $found = true;
-                        $line = trim($line);
-                        $this->assertStringEndsWith(' extends TestCase', $line);
+                        self::assertStringEndsWith(' extends TestCase', trim('' . $line));
 
                         break;
                     }
                 }
 
-                if (!$found) {
-                    $this->fail('No line starts with \'class\' in ' . $file);
+                if ($found === false) {
+                    self::fail('No line starts with \'class\' in ' . $file);
                 }
 
                 fclose($handle);
             } else {
-                $this->fail('Could not read ' . $file);
+                self::fail('Could not read ' . $file);
             }
         }
     }
 
-    public function endsWith(string $haystack, string $needle): bool
+    public static function endsWith(string $haystack, string $needle): bool
     {
         $length = strlen($needle);
 
@@ -68,7 +99,7 @@ class MetricsTest extends TestCase
         return substr($haystack, -$length) === $needle;
     }
 
-    public function startsWith(string $haystack, string $needle): bool
+    public static function startsWith(string $haystack, string $needle): bool
     {
         return strpos($haystack, $needle) === 0;
     }
