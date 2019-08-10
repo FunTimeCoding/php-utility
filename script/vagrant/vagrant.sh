3c71b0aa5d7c50dce5c83dbab01ebfe81908d3ab
Alexander Reitzel <funtimecoding@gmail.com>
2019-08-10 03:36:35 +0200

    ssh auth without accepting the host key first

--- script/vagrant/vagrant.sh
+++ script/vagrant/vagrant.sh
@@ -1,13 +1,14 @@
 #!/bin/sh -e
 
 if [ ! -f "${HOME}/.ssh/id_rsa" ]; then
-    ssh-keygen -C "vagrant@localhost" -N "" -f "${HOME}/.ssh/id_rsa"
-    cat "${HOME}/.ssh/id_rsa.pub" >> "${HOME}/.ssh/authorized_keys"
+    ssh-keygen -C "vagrant@localhost" -N "" -f "${HOME}/.ssh/id_ansible"
+    cat "${HOME}/.ssh/id_ansible.pub" >> "${HOME}/.ssh/authorized_keys"
     sudo mkdir -p -m 700 /root/.ssh
-    sudo cp "${HOME}/.ssh/id_rsa.pub" /root/.ssh/authorized_keys
+    sudo cp "${HOME}/.ssh/id_ansible.pub" /root/.ssh/authorized_keys
     sudo chmod 600 /root/.ssh/authorized_keys
 fi
 
+cp /vagrant/configuration/ssh.txt /home/vagrant/.ssh/config
 cp /vagrant/configuration/inputrc.txt /home/vagrant/.inputrc
 
 mkdir -p /home/vagrant/tmp
