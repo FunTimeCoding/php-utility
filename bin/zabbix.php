35025d608a48885973f9dda4dbce777a550e3625
Alexander Reitzel <funtimecoding@gmail.com>
2019-09-17 15:53:00 +0200

    add Zabbix API client example

---
+++ bin/zabbix.php
@@ -0,0 +1,16 @@
+#!/usr/bin/env php
+<?php
+
+namespace FunTimeCoding\PhpUtility\Bin;
+
+use FunTimeCoding\PhpUtility\LanguageExample\Zabbix\Client;
+
+require_once 'vendor/autoload.php';
+
+$client = new Client();
+
+try {
+    $client->main();
+} catch (\Exception $e) {
+    echo $e->getMessage() . PHP_EOL;
+}
