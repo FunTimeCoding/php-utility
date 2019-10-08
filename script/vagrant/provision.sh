c388e92218968637b6e1dad7a5b250aab767cec1
Alexander Reitzel <funtimecoding@gmail.com>
2019-10-08 17:20:50 +0200

    synchronize with skeleton

--- script/vagrant/provision.sh
+++ script/vagrant/provision.sh
@@ -12,10 +12,10 @@ elif [ "${CODENAME}" = stretch ]; then
     apt-get --quiet 2 update
     apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ruby-ronn dos2unix
     apt-get --quiet 2 install ansible --target-release stretch-backports
+
     apt-get --quiet 2 install apt-transport-https
     wget --no-verbose --output-document /etc/apt/trusted.gpg.d/sury.gpg https://packages.sury.org/php/apt.gpg
     echo "deb https://packages.sury.org/php stretch main" > /etc/apt/sources.list.d/sury.list
-    cp /vagrant/configuration/backports.txt /etc/apt/sources.list.d/backports.list
     apt-get --quiet 2 update
 
     # Set timezone for PHP.
@@ -57,5 +57,5 @@ elif [ "${CODENAME}" = stretch ]; then
 
     apt-get --quiet 2 install ansible --target-release stretch-backports
 elif [ "${CODENAME}" = buster ]; then
-    apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ruby-ronn ansible dos2unix
+    apt-get --quiet 2 install neovim multitail htop tree git shellcheck hunspell devscripts ruby-ronn dos2unix ansible
 fi
